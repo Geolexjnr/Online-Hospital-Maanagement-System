641a1e707e80c6ee7339de628ce1c8ec2b5fa91f
Geolexjnr <george.chileya@outlook.com>
2021-01-01 03:58:59 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -9,7 +9,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Dashboard</title>
+    <title>Admin-Dashboard</title>
 </head>
 
 <body>
