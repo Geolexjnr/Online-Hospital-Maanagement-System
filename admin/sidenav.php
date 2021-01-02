df9aff2b57821edfc369f0524fc5069b60088b15
Geolexjnr <george.chileya@outlook.com>
2021-01-02 03:15:51 +0200

    updated

--- admin/sidenav.php
+++ admin/sidenav.php
@@ -11,7 +11,7 @@
     <div class="list-group bg-info" style="height: 90vh;">
         <a href="index.php" class="list-group-item list-group-item-action bg-info 
                         text-center text-white">Dashboard</a>
-        <a href="" class="list-group-item list-group-item-action bg-info 
+        <a href="admin.php" class="list-group-item list-group-item-action bg-info 
                         text-center text-white">Administrators</a>
         <a href="" class="list-group-item list-group-item-action bg-info 
                         text-center text-white">Doctors</a>
